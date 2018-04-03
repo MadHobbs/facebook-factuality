c0ad02068a8001f844f6cc55f8b3e8dd24e88086
Madison Hobbs <mhobbs0760@scrippscollege.edu>
2018-04-02 19:14:15 -0700

    data exploration file created

---
+++ data_exploration.py
@@ -0,0 +1,17 @@
+"""
+Author      : Shota Yasunaga, Madison Hobbs, Justin Lauw
+Class       : HMC CS 158
+Date        : 2018 April 2
+Description : Project Data Exploration
+"""
+# python libraries
+import collections
+
+# numpy libraries
+import numpy as np
+import pandas as pd
+
+# matplotlib libraries
+import matplotlib.pyplot as plt
+
+# libraries specific to project
