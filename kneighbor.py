a04f29a47dea2e3aef4dbcbdd2080e51019642fa
CS70 Cricket <syasunaga@cs.hmc.edu>
2018-04-23 23:58:47 -0700

    kneighbor is working well???

--- kneighbor.py
+++ kneighbor.py
@@ -1,37 +1,30 @@
-from sklearn.neibhbors import NearestNeghbors
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import f1_score
 import numpy as np
+from util import make_test_train
 
 
-def tune(X_train, y_train, scoring):
-
-    # penalty: Regularization Term -> 'l2' or 'l1' or 'elasticnet'
-    penalty = [None, 'l2', 'l1', 'elasticnet']
-    # alpha: (float) constant that multiplies regulatrization term
-    alpha = [float(10.0**x) for x in np.linspace(-3, 3, num = 6+1)]
-    # max_iter: (int)
-    max_iter = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-
-    # Create the random grid
-    random_grid = {'penalty': penalty,
-                    'alpha': alpha,
-                    'max_iter': max_iter}
-
-    fracNeg = len(y_train[y_train == 0])/float(len(y_train))
-    weight = (1-fracNeg)/float(fracNeg) 
-    class_weight = {1:1, 0:weight}
-
-    perceptron = Perceptron(class_weight=class_weight)
-
-    perceptron_random = \
-        RandomizedSearchCV(estimator = perceptron, param_distributions = random_grid, \
-                            n_iter = 100, cv = 3, verbose=2, random_state=42, \
-                            n_jobs = -1, scoring=scoring)
-    
-    perceptron_random.fit(X_train, y_train)
-    return perceptron_random.best_params_
+def best_n(X_train, X_test,y_train,y_test,min_n = 1, max_n = 19):
+    f1_list = []
+    for i in range(min_n,max_n+1):
+        nbrs = NearestNeighbors(n_neighbors=i,algorithm='ball_tree').fit(X_train)
+        distances, indices = nbrs.kneighbors(X_test)
+        pred = []
+        for ind in indices:
+            y_list = y_train[ind]
+            pred.append(np.bincount(y_list).argmax())
+        f1_list.append(f1_score(y_test,pred))
+    print max_n
+    print f1_list[np.argmax(f1_list)]
+    return range(min_n,max_n+1)[np.argmax(f1_list)]
 
 def main():
-    pass
+    X_train,X_test,y_train,y_test = make_test_train()
+    y_train = y_train.astype('int64')
+    y_test = y_test.astype('int64') 
+    print best_n(X_train,X_test,y_train,y_test,max_n = 55)
+
+    #nbrs  = NearestNeghbors(n_neighbors = )
 
 if __name__ == '__main__':
     main()
